e11ea50fd3d1ed019543c61922d238c8cf5ce705
jun112465 <ktop101777@gmail.com>
2022-03-23 00:40:10 +0900

    props and state 기본 학습

--- App.js
+++ App.js
@@ -1,5 +1,36 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+//to use JSX
+import React, {useState} from 'react';
+//to use components
+import { Text, View, StyleSheet, Button} from 'react-native';
+
+const styles = StyleSheet.create({
+    center: {
+        alignItems: 'center',
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
+
+const Greeting = (props) => {
+    return (
+        <View style={styles.center}>
+            <Text>Hello {props.name}</Text>
+        </View>
+    )
+}
+
+const LotsOfGreetings = ()=>{
+    return (
+        <View style={[styles.center, {top:50}]}>
+            <Greeting name='JunYeob'/>
+            <Greeting name='JunSung'/>
+            <Greeting name='YoonSung'/>
+        </View>
+    )
+}
 
 const HelloWorldApp = () => {
   return (
@@ -13,4 +44,19 @@ const HelloWorldApp = () => {
     </View>
   )
 }
-export default HelloWorldApp;
+
+const App = ()=>{
+    const [count, setCount] = useState(0);
+
+    return (
+        <View style={styles.container}>
+            <Text>You clicked {count} times</Text>
+            <Button 
+                onPress={()=>setCount(count + 1)}
+                title="click me!"
+            />
+        </View>
+    )
+}
+// export default LotsOfGreetings;
+export default App;
