5ddd6514b1c677d110619f3bfba5828582145943
jun112465 <ktop101777@gmail.com>
2022-03-27 00:55:14 +0900

    storage test

--- App.js
+++ App.js
@@ -1,67 +1,110 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import React, {useState, useEffect} from "react";
+import {StyleSheet, Text, View, Image, TextInput, TouchableOpacity} from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import HomeScreen from './components/HomeScreen';
-import SettingsScreen from './components/SettingsScreen';
-import StorageTest from './components/StorageTest';
-// function homeScreen() {
-//   return (
-//       <HomeScreen />
-// //     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-// //       <Text>Home!</Text>
-// //     </View>
-//   );
-// }
 
-// function SettingsScreen() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Text>Settings!</Text>
-//     </View>
-//   );
-// }
+export default function App(){
+  const [name, setName] = useState();
 
-const Tab = createBottomTabNavigator();
+  const save = async () => {
+    try{
+      await AsyncStorage.setItem("MyName", name);
+    }catch(err){  
+      alert(err);
+    }
 
-function MyTabs(size, color) {
+    let user={
+      name: "DesignIntoCOde",
+      location: "US",
+    }
+    // await AsyncStorage.setItem("MyName", JSON.stringify(user));
+  }
+
+  const load = async () => {
+    try{
+      let name = await AsyncStorage.getItem("MyName");
+      if(name !== null){
+        setName(name);
+      }
+
+      // let jsonValue = await AsyncStorage.getItem("MyName");
+      // if(jsonValue != null){
+      //   setName(JSON.parse(jsonValue));
+      // }
+    }catch(err){
+      alert(err);
+    }
+  }
+
+  const remove = async () =>{
+    try{
+      await AsyncStorage.removeItem("MyName");
+    }catch{
+      alert(err);
+    }finally{
+      setName("");
+    }
+  }
+
+  useEffect(()=>{
+    load();
+  }, []);
+
   return (
-    <Tab.Navigator
-      screenOptions={{ headerShown: false }}
-    >
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          tabBarIcon: ({ size, color }) => (<Icon name={"home"} color={color} size={size} />)
-        }}
-      />
+    <View style={styles.container}>
+      <Image 
+      source={(require("./assets/instagram-logo.png"))} 
+      style={{width:"100%", height:200, marginTop: 64}}
+      resizeMode="contain"
+      /> 
 
-      <Tab.Screen
-        name="Storage"
-        component={StorageTest}
-        options={{
-          tabBarIcon: ({ size, color }) => (<Icon name={"user"} color={color} size={size} />)
-        }}
-      />
+      <Text style={{height:30}}>{name}</Text>
+      <Text style={styles.name}>What's your name?</Text>
 
-      <Tab.Screen
-        name="Settings"
-        component={SettingsScreen}
-        options={{
-          tabBarIcon: ({ size, color }) => (<Icon name={"user"} color={color} size={size} />)
-        }}
-      />
-    </Tab.Navigator>
-  );
+      <TextInput style={styles.input} onChangeText={(text)=>setName(text)}/>
+
+      <TouchableOpacity style={styles.button} onPress={()=>save()}>
+        <Text style={{color: "white"}}>Save my Name</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button} onPress={()=>remove()}>
+        <Text style={{color: "white"}}>Remove my Name</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <MyTabs/>
-    </NavigationContainer>
-  );
-}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center"
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: "300",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#575DD9",
+    alignSelf: "stretch",
+    margin: 32,
+    height: 64,
+    borderRadius: 6,
+    paddingHorizontal: 16,
+    fontSize: 24,
+    fontWeight: "300",
+  },
+  button: {
+    backgroundColor: "#575DD9", 
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "stretch",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    marginTop: 32,
+    marginHorizontal: 32,
+    borderRadius: 6,
+  }
+})
