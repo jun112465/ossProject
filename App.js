5ae40e2086f67abc0124005b84c26418a8bbb6b2
jun112465 <ktop101777@gmail.com>
2022-03-29 05:58:07 +0900

    commit

--- App.js
+++ App.js
@@ -1,41 +1,11 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import HomeScreen from './components/HomeScreen';
-import SettingsScreen from './components/SettingsScreen';
+import MyStack from './MyStack';
 
-const Tab = createBottomTabNavigator();
-
-function MyTabs(size, color) {
+const App = () => {
   return (
-      <Tab.Navigator
-          screenOptions={{ headerShown: false }}
-      >
-          <Tab.Screen
-              name="Home"
-              component={HomeScreen}
-              options={{
-                  tabBarIcon: ({ size, color }) => (<Icon name={"home"} color={color} size={size} />)
-              }}
-          />
-
-          <Tab.Screen
-              name="Settings"
-              component={SettingsScreen}
-              options={{
-                  tabBarIcon: ({ size, color }) => (<Icon name={"user"} color={color} size={size} />)
-              }}
-          />
-      </Tab.Navigator>
+    <MyStack/>
   );
-}
+};
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <MyTabs/>
-    </NavigationContainer>
-  );
-}
+export default App;
