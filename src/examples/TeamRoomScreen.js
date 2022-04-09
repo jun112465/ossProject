8ff750f0210c7bbcbd7ed46120332aa7404ddf15
jun112465 <ktop101777@gmail.com>
2022-04-09 19:47:38 +0900

    room screen

---
+++ src/examples/TeamRoomScreen.js
@@ -0,0 +1,13 @@
+import react from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Room = ()=>{
+
+    return (
+        <SafeAreaView>
+            <Text>Room A</Text>
+            
+        </SafeAreaView>
+    )
+}
