8089417678162a2c284fbba05e778f3d5b04fda4
jun112465 <ktop101777@gmail.com>
2022-04-12 20:12:16 +0900

    kakaoLogin 50% complete

---
+++ ios/bh.swift
@@ -0,0 +1,8 @@
+//
+//  bh.swift
+//  ossProject
+//
+//  Created by Jun on 2022/04/12.
+//
+
+import Foundation
--- ios/ossProject/AppDelegate.m
+++ ios/ossProject/AppDelegate.m
@@ -11,6 +11,17 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+#import <RNKakaoLogins.h>
+
+- (BOOL)application:(UIApplication *)app
+     openURL:(NSURL *)url
+     options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+ if([RNKakaoLogins isKakaoTalkLoginUrl:url]) {
+    return [RNKakaoLogins handleOpenUrl: url];
+ }
+
+ return NO;
+}
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
