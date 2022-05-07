3d33b26db113d3e469e1c7a9ee5a2aeb86895f7c
jun112465 <ktop101777@gmail.com>
2022-05-07 17:19:01 +0900

    일정등록 화면 제작중

--- src/screens/mainScreens/teams/TeamRoomScreen.js
+++ src/screens/mainScreens/teams/TeamRoomScreen.js
@@ -11,6 +11,26 @@ const TeamRoom = ({route, navigation})=>{
         alignItems: 'center'}}>
             <Text style={{padding: 32, alignContent:"center", fontSize:24}}>{route.params.teamName}</Text>
             <Calendar
+            markingType={'period'}
+            markedDates={{
+                // '2022-05-16': {selected: true, marked: true, selectedColor: 'blue'},
+                // '2022-05-17': {marked: true},
+                // '2022-05-18': {marked: true, dotColor: 'red', activeOpacity: 0},
+                // '2022-05-19': {disabled: true, disableTouchEvent: true}
+
+                '2022-05-20': {textColor: 'green'},
+                '2022-05-22': {startingDay: true, color: 'green'},
+                '2022-05-23': {selected: true, endingDay: true, color: 'green', textColor: 'gray'},
+                '2022-05-04': {startingDay: true, color: 'green', endingDay: true},
+
+                '2022-05-15': {marked: true, dotColor: '#50cebb'},
+                '2022-05-16': {marked: true, dotColor: '#50cebb'},
+                '2022-05-21': {startingDay: true, color: '#50cebb', textColor: 'white'},
+                '2022-05-22': {color: '#70d7c7', textColor: 'white'},
+                '2022-05-23': {color: '#70d7c7', textColor: 'white', marked: true, dotColor: 'white'},
+                '2022-05-24': {color: '#70d7c7', textColor: 'white'},
+                '2022-05-25': {endingDay: true, color: '#50cebb', textColor: 'white'}
+              }}
             style={{
                 borderWidth: 1,
                 borderColor: 'gray',
